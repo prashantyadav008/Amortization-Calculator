8e0ac6bdbcd6d9de6de76bd33025b2b545835172
itsprashant008 <pkpyadav008@gmail.com>
2024-02-26 00:17:28 +0530

    Update EMI Calcualator form design

--- react_client/src/components/pages/EMICalculator.jsx
+++ react_client/src/components/pages/EMICalculator.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 
+import "../../assets/css/jquery-ui.css";
+import "../../assets/css/calculator.css";
+
 export const EMICalculator = () => {
   const [loanAmount, setLoanAmount] = useState(2500000); // Default: ₹25,00,000
   const [interestRate, setInterestRate] = useState(10.5); // Default: 10.5%
@@ -40,138 +43,318 @@ export const EMICalculator = () => {
   };
 
   return (
-    <form id="emicalculatorform">
-      <div className="form-horizontal" id="emicalculatorinnerform">
-        {/* Loan Amount */}
-        <div className="row form-group lamount">
-          <label className="col-lg-4 control-label" htmlFor="loanamount">
-            Home Loan Amount
-          </label>
-          <div className="col-lg-6">
-            <div className="input-group">
-              <input
-                className="form-control"
-                id="loanamount"
-                name="loanamount"
-                value={loanAmount}
-                type="tel"
-                onChange={handleLoanAmountChange}
-              />
-              <div className="input-group-append">
-                <span className="input-group-text">$/₹</span>
-              </div>
-            </div>
-            <input
-              type="range"
-              min="0"
-              max="5000000"
-              value={loanAmount}
-              onChange={(e) => setLoanAmount(parseInt(e.target.value))}
-            />
-          </div>
-        </div>
-        {/* Interest Rate */}
-        <div className="sep row form-group lint">
-          <label className="col-lg-4 control-label" htmlFor="loaninterest">
-            Interest Rate
-          </label>
-          <div className="col-lg-6">
-            <div className="input-group">
-              <input
-                type="tel"
-                className="form-control"
-                id="loaninterest"
-                name="loaninterest"
-                value={interestRate}
-                onChange={handleInterestRateChange}
-              />
-              <div className="input-group-append">
-                <span className="input-group-text">%</span>
-              </div>
-            </div>
-            <input
-              type="range"
-              min="0"
-              max="100"
-              step="0.1"
-              value={interestRate}
-              onChange={(e) => setInterestRate(parseFloat(e.target.value))}
-            />
-          </div>
-        </div>
-        {/* Loan Tenure */}
-        <div className="sep row form-group lterm">
-          <label className="col-lg-4 control-label" htmlFor="loanterm">
-            Loan Tenure
-          </label>
-          <div className="col-lg-6">
-            <div className="loantermwrapper">
-              <div className="input-group">
-                <input
-                  className="form-control"
-                  id="loanterm"
-                  name="loanterm"
-                  value={loanTenure.value}
-                  type="tel"
-                  onChange={(e) =>
-                    setLoanTenure({ ...loanTenure, value: e.target.value })
-                  }
-                />
-                <div
-                  className="input-group-append$/ tenure-choice"
-                  data-toggle="buttons">
-                  <div
-                    className="btn-group btn-group-toggle"
-                    data-toggle="buttons">
-                    <label
-                      className={`btn btn-secondary ${
-                        loanTenure.unit === "year" ? "active" : ""
-                      }`}>
-                      <input
-                        type="radio"
-                        name="loantenure"
-                        value="year"
-                        tabIndex="4"
-                        autoComplete="off"
-                        checked={loanTenure.unit === "year"}
-                        onChange={handleLoanTenureChange}
-                      />
-                      Yr
-                    </label>
-                    <label
-                      className={`btn btn-secondary ${
-                        loanTenure.unit === "month" ? "active" : ""
-                      }`}>
-                      <input
-                        type="radio"
-                        name="loantenure"
-                        value="month"
-                        tabIndex="5"
-                        autoComplete="off"
-                        checked={loanTenure.unit === "month"}
-                        onChange={handleLoanTenureChange}
-                      />
-                      Mo
-                    </label>
+    <>
+      <div className="calculatorcontainer">
+        <div className="emicalculatorcontainer">
+          <div id="loanformcontainer" className="row">
+            <div id="emicalculatordashboard" className="col-sm-12">
+              <div id="emicalculatorinnerformwrapper">
+                <form id="emicalculatorform">
+                  <div className="form-horizontal" id="emicalculatorinnerform">
+                    {/* Loan Amount */}
+                    <div className="row form-group lamount">
+                      <label
+                        className="col-lg-4 control-label"
+                        for="loanamount">
+                        Loan Amount
+                      </label>
+
+                      <div className="col-lg-6">
+                        <div className="input-group">
+                          <input
+                            className="form-control"
+                            id="loanamount"
+                            name="loanamount"
+                            value={loanAmount}
+                            type="tel"
+                            onChange={handleLoanAmountChange}
+                          />
+                          <div className="input-group-append">
+                            <span className="input-group-text">$/₹</span>
+                          </div>
+                        </div>
+                        <input
+                          type="range"
+                          min="0"
+                          max="5000000"
+                          value={loanAmount}
+                          onChange={(e) =>
+                            setLoanAmount(parseInt(e.target.value))
+                          }
+                        />
+                      </div>
+                    </div>
+                    <div className="loanAmountSlider">
+                      <div
+                        id="loanamountslider"
+                        className="ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content">
+                        <div
+                          className="ui-slider-range ui-corner-all ui-widget-header ui-slider-range-min"
+                          style={{ width: "100%" }}></div>
+                        <span
+                          tabIndex="0"
+                          className="ui-slider-handle ui-corner-all ui-state-default"
+                          style={{ left: "100%" }}></span>
+                      </div>
+
+                      <div id="loanamountsteps" className="steps">
+                        <span className="tick" style={{ left: "0%" }}>
+                          |<br />
+                          <span className="marker">0</span>
+                        </span>
+                        <span
+                          className="tick d-none d-sm-table-cell"
+                          style={{ left: "12.5%" }}>
+                          |<br />
+                          <span className="marker">25L</span>
+                        </span>
+                        <span className="tick" style={{ left: "25%" }}>
+                          |<br />
+                          <span className="marker">50L</span>
+                        </span>
+                        <span
+                          className="tick d-none d-sm-table-cell"
+                          style={{ left: "37.5%" }}>
+                          |<br />
+                          <span className="marker">75L</span>
+                        </span>
+                        <span className="tick" style={{ left: "50%" }}>
+                          |<br />
+                          <span className="marker">100L</span>
+                        </span>
+                        <span
+                          className="tick d-none d-sm-table-cell"
+                          style={{ left: "62.5%" }}>
+                          |<br />
+                          <span className="marker">125L</span>
+                        </span>
+                        <span className="tick" style={{ left: "75%" }}>
+                          |<br />
+                          <span className="marker">150L</span>
+                        </span>
+                        <span
+                          className="tick d-none d-sm-table-cell"
+                          style={{ left: "87.5%" }}>
+                          |<br />
+                          <span className="marker">175L</span>
+                        </span>
+                        <span className="tick" style={{ left: "100%" }}>
+                          |<br />
+                          <span className="marker">200L</span>
+                        </span>
+                      </div>
+                    </div>
+
+                    {/* Interest Rate */}
+                    <div className="sep row form-group lint">
+                      <label
+                        className="col-lg-4 control-label"
+                        htmlFor="loaninterest">
+                        Interest Rate
+                      </label>
+
+                      <div className="col-lg-6">
+                        <div className="input-group">
+                          <input
+                            type="tel"
+                            className="form-control"
+                            id="loaninterest"
+                            name="loaninterest"
+                            value={interestRate}
+                            onChange={handleInterestRateChange}
+                          />
+                          <div className="input-group-append">
+                            <span className="input-group-text">%</span>
+                          </div>
+                        </div>
+                        <input
+                          type="range"
+                          min="0"
+                          max="100"
+                          step="0.1"
+                          value={interestRate}
+                          onChange={(e) =>
+                            setInterestRate(parseFloat(e.target.value))
+                          }
+                        />
+                      </div>
+                    </div>
+
+                    <div className="loanInterestSlider">
+                      <div
+                        id="loaninterestslider"
+                        className="ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content">
+                        <div
+                          className="ui-slider-range ui-corner-all ui-widget-header ui-slider-range-min"
+                          style={{ width: "26.6667%" }}></div>
+                        <span
+                          tabIndex="0"
+                          className="ui-slider-handle ui-corner-all ui-state-default"
+                          style={{ left: "26.6667%" }}></span>
+                      </div>
+
+                      <div id="loanintereststeps" className="steps">
+                        <span className="tick" style={{ left: "0%" }}>
+                          |<br />
+                          <span className="marker">5</span>
+                        </span>
+                        <span className="tick" style={{ left: "16.67%" }}>
+                          |<br />
+                          <span className="marker">7.5</span>
+                        </span>
+                        <span className="tick" style={{ left: "33.34%" }}>
+                          |<br />
+                          <span className="marker">10</span>
+                        </span>
+                        <span className="tick" style={{ left: "50%" }}>
+                          |<br />
+                          <span className="marker">12.5</span>
+                        </span>
+                        <span className="tick" style={{ left: "66.67%" }}>
+                          |<br />
+                          <span className="marker">15</span>
+                        </span>
+                        <span className="tick" style={{ left: "83.34%" }}>
+                          |<br />
+                          <span className="marker">17.5</span>
+                        </span>
+                        <span className="tick" style={{ left: "100%" }}>
+                          |<br />
+                          <span className="marker">20</span>
+                        </span>
+                      </div>
+                    </div>
+
+                    {/* Loan Tenure */}
+                    <div className="sep row form-group lterm">
+                      <label
+                        className="col-lg-4 control-label"
+                        htmlFor="loanterm">
+                        Loan Tenure
+                      </label>
+
+                      <div className="col-lg-6">
+                        <div className="loantermwrapper">
+                          <div className="input-group">
+                            <input
+                              className="form-control"
+                              id="loanterm"
+                              name="loanterm"
+                              value={loanTenure.value}
+                              type="tel"
+                              onChange={(e) =>
+                                setLoanTenure({
+                                  ...loanTenure,
+                                  value: e.target.value,
+                                })
+                              }
+                            />
+                            <div
+                              className="input-group-append "
+                              data-toggle="buttons">
+                              <div
+                                className="btn-group btn-group-toggle"
+                                data-toggle="buttons">
+                                <label
+                                  className={`btn btn-light ${
+                                    loanTenure.unit === "year" ? "active" : ""
+                                  }`}>
+                                  <input
+                                    type="radio"
+                                    name="loantenure"
+                                    value="year"
+                                    tabIndex="4"
+                                    autoComplete="off"
+                                    checked={loanTenure.unit === "year"}
+                                    onChange={handleLoanTenureChange}
+                                  />
+                                  Yr
+                                </label>
+                                <label
+                                  className={`btn btn-light ${
+                                    loanTenure.unit === "month" ? "active" : ""
+                                  }`}>
+                                  <input
+                                    type="radio"
+                                    name="loantenure"
+                                    value="month"
+                                    tabIndex="5"
+                                    autoComplete="off"
+                                    checked={loanTenure.unit === "month"}
+                                    onChange={handleLoanTenureChange}
+                                  />
+                                  Mo
+                                </label>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <input
+                          type="range"
+                          min="0"
+                          max="30"
+                          value={loanTenure.value}
+                          onChange={(e) =>
+                            setLoanTenure({
+                              ...loanTenure,
+                              value: parseInt(e.target.value),
+                            })
+                          }
+                        />
+                      </div>
+                    </div>
+
+                    <div className="loanTermSlider">
+                      <div
+                        id="loantermslider"
+                        className="ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content">
+                        <div
+                          className="ui-slider-range ui-corner-all ui-widget-header ui-slider-range-min"
+                          style={{ width: "66.6667%" }}></div>
+                        <span
+                          tabIndex="0"
+                          className="ui-slider-handle ui-corner-all ui-state-default"
+                          style={{ left: "66.6667%" }}></span>
+                      </div>
+
+                      <div id="loantermsteps" className="steps">
+                        <span className="tick" style={{ left: "0%" }}>
+                          |<br />
+                          <span className="marker">0</span>
+                        </span>
+                        <span className="tick" style={{ left: "16.67%" }}>
+                          |<br />
+                          <span className="marker">5</span>
+                        </span>
+                        <span className="tick" style={{ left: "33.33%" }}>
+                          |<br />
+                          <span className="marker">10</span>
+                        </span>
+                        <span className="tick" style={{ left: "50%" }}>
+                          |<br />
+                          <span className="marker">15</span>
+                        </span>
+                        <span className="tick" style={{ left: "66.67%" }}>
+                          |<br />
+                          <span className="marker">20</span>
+                        </span>
+                        <span className="tick" style={{ left: "83.33%" }}>
+                          |<br />
+                          <span className="marker">25</span>
+                        </span>
+                        <span className="tick" style={{ left: "100%" }}>
+                          |<br />
+                          <span className="marker">30</span>
+                        </span>
+                      </div>
+                    </div>
                   </div>
-                </div>
+                </form>
               </div>
             </div>
-            <input
-              type="range"
-              min="0"
-              max="30"
-              value={loanTenure.value}
-              onChange={(e) =>
-                setLoanTenure({
-                  ...loanTenure,
-                  value: parseInt(e.target.value),
-                })
-              }
-            />
           </div>
         </div>
       </div>
-    </form>
+    </>
   );
 };
