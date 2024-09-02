b62fb7d8e6e6fde3bbd287054fde41d8330640bb
prashantyadav008 <prashantsdbct009@gmail.com>
2024-09-02 23:43:19 +0530

    Update Infura Key and Remove Dependency for Sepolia

--- react_client/src/components/smart_contract/ContractMethods.jsx
+++ react_client/src/components/smart_contract/ContractMethods.jsx
@@ -3,35 +3,20 @@
 import { Web3 } from "web3";
 import EMI_ABI from "./EMI_ABI.json";
 
-const Web3Index = async () => {
-  // eslint-disable-next-line no-async-promise-executor
-  return new Promise(async (resolve, reject) => {
-    const web3 = new Web3(window.ethereum);
-
-    const chainId = (await web3.eth.getChainId()).toString();
-
-    // eslint-disable-next-line no-undef
-    if (chainId !== process.env.REACT_APP_ChainId) {
-      var modal = document.getElementById("myModal");
-      modal.style.display = "block";
-    } else {
-      try {
-        resolve(web3);
-      } catch (error) {
-        reject(error);
-      }
-    }
-  });
-};
-
 const ContractInstance = async () => {
   // eslint-disable-next-line no-undef
-  let emiContract = process.env.EMI_Contract;
+  let emiContract = process.env.REACT_APP_EMI_Contract;
   let emiAbi = EMI_ABI;
 
-  const web3 = await Web3Index();
+  // eslint-disable-next-line no-undef
+  const providerUrl = process.env.REACT_APP_ALCHEMY_SEPOLIA_API_KEY;
+
+  // Create a new Web3 instance using the provider URL
+  const web3 = new Web3(new Web3.providers.HttpProvider(providerUrl));
 
   let emi = new web3.eth.Contract(emiAbi, emiContract);
+
+  console.log(emi);
 
   return {
     emi: emi,
